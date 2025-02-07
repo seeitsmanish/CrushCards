775119281283abdd00f9409f07c910b3fcc71953
See-Its-Manish <seeitsmanish@gmail.com>
2025-02-07 21:09:16 +0530

    fix: deployment

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 module.exports = nextConfig;
